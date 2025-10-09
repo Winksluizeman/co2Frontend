416f5f236aa0d51af8d1396de09390ff5910a677
WinkSluizeman53 <563017@student.fontys.nl>
2025-10-09 14:39:18 +0200

    First forms for account

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    server: {
+        proxy: {
+            '/api': {
+                target: 'http://localhost:8080', // je Spring Boot backend
+                changeOrigin: true,              // zorgt dat host headers kloppen
+                secure: false,                   // handig als je later met https werkt
+            }
+        }
+    }
 })

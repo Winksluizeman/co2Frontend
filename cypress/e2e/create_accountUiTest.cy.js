c7cfd9ee48a401e4d78d7ec10740784ff734ed19
WinkSluizeman53 <563017@student.fontys.nl>
2026-01-09 18:10:21 +0100

    ci pipeline is functional, bug fix for the ui test

--- cypress/e2e/create_accountUiTest.cy.js
+++ cypress/e2e/create_accountUiTest.cy.js
@@ -1,18 +1,27 @@
+describe('Create Account UI Test (Mocked Backend)', () => {
 
-describe('Create Account UI Test', () => {
+    it('should allow the user to create an account', () => {
 
-  it('should allow the user to create an account', () => {
+        // 🔥 Mock de backend zodat Cypress geen echte API nodig heeft
+        cy.intercept('POST', 'http://localhost:8080/register', {
+            statusCode: 200,
+            body: { message: "ok" }
+        }).as('mockRegister');
 
-    cy.visit('/register');
+        cy.visit('/register');
 
-    cy.get('input[name="username"]').type('Jeroen');
-    cy.get('input[name="age"]').type('21');
-    cy.get('input[name="email"]').type('Jeroen@Gmail.com');
-    cy.get('input[name="password"]').type('Password123');
+        cy.get('input[name="username"]').type('Jeroen');
+        cy.get('input[name="age"]').type('21');
+        cy.get('input[name="email"]').type('Jeroen@Gmail.com');
+        cy.get('input[name="password"]').type('Password123');
 
-    cy.get('button[type="submit"]').click();
+        cy.get('button[type="submit"]').click();
 
-    cy.contains('Account successfully created').should('be.visible');
-  });
+        // Wacht op de mock-response
+        cy.wait('@mockRegister');
+
+        // Check of de success-tekst verschijnt
+        cy.contains('Account successfully created').should('be.visible');
+    });
 
 });
